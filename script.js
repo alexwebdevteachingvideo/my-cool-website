e75b3cd33d7a4a65625378d5ff7c8a3aaf7ef6ff
Alex Eagleson <alexeagleson@gmail.com>
2022-08-12 12:30:47 -0400

    implement local storage for tracking likes

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
+const savedLikes = localStorage.getItem('likes');
+
 let numberOfLikes = 0;
+
+if (savedLikes === null) {
+  // do nothing
+} else if (typeof savedLikes === "string") {
+  numberOfLikes = Number(savedLikes);
+}
+
+const button = document.querySelector("#like-button");
+if (button !== null) {
+  button.innerHTML = "👍 " + numberOfLikes;
+}
 
 function onLikeButtonClick() {
   const likeButton = document.querySelector("#like-button");
 
   numberOfLikes = numberOfLikes + 1;
+  localStorage.setItem('likes', numberOfLikes);
 
   if (likeButton !== null) {
     likeButton.innerHTML = "👍 " + numberOfLikes;
